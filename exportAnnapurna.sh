fe054c2e05c37f053c117d7bcd4e408ab9d24dfc
chrischan <christian.linder@gmail.com>
2015-02-01 17:07:31 +0100

    moved nepal related stuff out of this

--- exportAnnapurna.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export.js --sim --left 83.4 --right 85 --bottom 28 --top 29 --minzoom 16 --maxzoom 18
--- exportNepal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export.js --left 80 --right 89 --bottom 26 --top 31 --minzoom 0 --maxzoom 16
